b611fd9864fdfde67a83e309a4aabc5d75140dbb
n3lls <nenes93150@gmail.com>
2025-05-12 15:03:34 +0200

    Second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //Exercice 1
+        String nom = "san" ;
+        String prenom="nel";
+        System.out.println("Bonjour " + prenom +" "+nom);
+
+        //exercice 2
+        int a=3;
+        int b=2;
+        System.out.println("avant permutation: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        int c= b;
+        b = a;
+        a = c;
+        System.out.println("apres permutation: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        int result = (a*a) + (b*b);
+        System.out.println(" La somme des carrés de a et b est :"+ result );
     }
 }
